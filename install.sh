9b90ce42c2bed18445265ba512bc5060bde5d56b
Mauro Ghiglia <mauroghiglia@gmail.com>
2025-04-17 12:24:31 +0300

    Finished install.sh

--- install.sh
+++ install.sh
@@ -17,8 +17,18 @@ sudo chmod +x /usr/local/bin/logengine-stop
 sudo chmod +x /usr/local/bin/logengine-status
 
 # Make program files executable
-# sudo chmod +x program/logengine.py
-# sudo chmod +x program/logengine.config.yaml
+sudo chmod +x program/logengine.py
+sudo chmod +x program/logengine.config.yaml
 
-# echo "lg-start, lg-stop, and og-status have been installed to /usr/local/bin."
-# echo "You can now use them from anywhere in the terminal."
+# Create a directory for the logengine log files
+sudo mkdir -p /var/log/logengine-public
+sudo chmod +x /var/log/logengine-public
+sudo chown $ubuntu:$ubuntu /var/log/logengine-public
+
+sudo mkdir -p /var/log/logengine-public-logs
+sudo chmod +x /var/log/logengine-public-logs
+sudo chown $ubuntu:$ubuntu /var/log/logengine-public-logs
+
+echo "lg-start, lg-stop, and og-status have been installed to /usr/local/bin."
+echo "A folder named /var/log/logengine-public has been created for log files."
+echo "You can view the logs in /var/log/logengine-public-logs."
