471a805e49ec7ed5a6b00357065d25479a373b53
Mauro Ghiglia <mauroghiglia@gmail.com>
2025-04-24 17:33:00 +0300

    Changes to logengine-stop

--- commands/logengine-stop.sh
+++ commands/logengine-stop.sh
@@ -1,7 +1,7 @@
 # This section stops the log-generator
 # logengine.py stop
 
-python3 ~/projects/logengine-public/program/logengine.py stop &
+python3 ~/projects/logengine-public/program/logengine.py stop
 
 # Force a clean newline and prompt redraw
 sleep 0.2
